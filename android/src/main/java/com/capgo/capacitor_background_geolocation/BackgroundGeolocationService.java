deb332ce774f2a6ddfdd1a03221dad4f4695e5d2
Harel M <harel.mazor@gmail.com>
2026-01-04 12:37:04 +0000

    Add missing import

--- android/src/main/java/com/capgo/capacitor_background_geolocation/BackgroundGeolocationService.java
+++ android/src/main/java/com/capgo/capacitor_background_geolocation/BackgroundGeolocationService.java
@@ -5,6 +5,7 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ServiceInfo;
 import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.graphics.Color;
