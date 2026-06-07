487cae8110539729206ea89905de4bbc3c3cde1c
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 13:13:31 +0200

    style: format capgo example rollout

--- example-app/capacitor.config.ts
+++ example-app/capacitor.config.ts
@@ -3,19 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 import pkg from './package.json';
 
 const config: CapacitorConfig = {
-  "appId": "com.capgo.backgroundgeolocation.example",
-  "appName": "Background Geolocation Example",
-  "webDir": "dist",
-  "plugins": {
-    "CapacitorUpdater": {
-      "appId": "com.capgo.backgroundgeolocation.example",
-      "autoUpdate": true,
-      "autoSplashscreen": true,
-      "directUpdate": "always",
-      "defaultChannel": "production",
-      "version": pkg.version
-    }
-  }
+  appId: 'com.capgo.backgroundgeolocation.example',
+  appName: 'Background Geolocation Example',
+  webDir: 'dist',
+  plugins: {
+    CapacitorUpdater: {
+      appId: 'com.capgo.backgroundgeolocation.example',
+      autoUpdate: true,
+      autoSplashscreen: true,
+      directUpdate: 'always',
+      defaultChannel: 'production',
+      version: pkg.version,
+    },
+  },
 };
 
 export default config;
--- example-app/src/main.js
+++ example-app/src/main.js
@@ -22,7 +22,8 @@ function log(text, time = Date.now(), color = 'gray') {
   container.insertBefore(li, container.firstChild);
 }
 
-document.querySelector('#app').innerHTML = '<h1>Background Geolocation</h1><div><ul id="watchers"></ul><button id="addFG">Add FG Watcher</button><button id="addBG">Add BG Watcher</button><button id="requestPerms">Request Permissions</button></div><ul id="log"><li>Init</li></ul>';
+document.querySelector('#app').innerHTML =
+  '<h1>Background Geolocation</h1><div><ul id="watchers"></ul><button id="addFG">Add FG Watcher</button><button id="addBG">Add BG Watcher</button><button id="requestPerms">Request Permissions</button></div><ul id="log"><li>Init</li></ul>';
 
 document.getElementById('addFG').onclick = () => log('FG watcher clicked');
 document.getElementById('addBG').onclick = () => log('BG watcher clicked');
